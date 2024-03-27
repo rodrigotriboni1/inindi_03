a7329b60ada326ddf50fa677542b4c94f1f3e16a
josuemoraisgh <josuemorais@gmail.com>
2024-03-27 13:33:23 -0300

    refactor inind.h

--- include/inindThread.h
+++ include/inindThread.h
@@ -1,72 +1,46 @@
-// https://blog.eletrogate.com/interrupcao-o-que-e-como-utilizar-arduin/
-// https://www.squids.com.br/arduino/index.php/projetos-arduino/projetos-squids/basico/263-projeto-79-interrupcao-por-timer-no-arduino-alarme-sensor-de-toque
-
 // O timer0 é utilizado pelo Arduino para funções como delay(), millis() e micros(). Então não se deve utilizar esse timer para evitar comprometer essas funções.
 // O timer1 no Arduino UNO esse é o timer utilizado pela biblioteca de controle de servos. Caso você não esteja utilizando essa biblioteca, esse timer está livre para ser utilizado para outros propósitos. No Arduino Mega esse timer só será utilizado para controlar os servos se você estiver usando mais de 12 servos. (timer utilizado no projeto)
-// O timer2 é utilizado pela função tone(). Então se você não precisar da função tone() esse timer está livre para outras aplicações.
+// O timer2 é utilizado pela função tone(). Então se você não precisar da função tone() esse timer está livre para outras aplicações
 #include <Arduino.h>
+#include <TimerOne.h>
 #include <stdarg.h>
 
-#define TAMTHREAD 5 
-using CallbackFunc = void (*)(); // definição de tipo para nossa callback
-struct threadStruct
-{
-  volatile CallbackFunc func[TAMTHREAD];
-  volatile int interval[TAMTHREAD];
-  volatile int time[TAMTHREAD];
-};
-threadStruct thread;
-volatile int numThread;
+#ifndef NUMTHREADS
+#define NUMTHREADS 1
+#endif
+struct threadStruct {
+  void (*func)(); //Função executada pela thread
+  int interval;//Instante atual da thread
+  int time; //Altera o tempo da thread
+} thread[NUMTHREADS];
 
-void threadSetup(CallbackFunc callback, int threadInterval, ...)
-{
-  numThread = 1;
+void threads() {
+  for (int i = 0; i < NUMTHREADS; i++) {
+    if (thread[i].time >= thread[i].interval) {
+      thread[i].func();
+      thread[i].time = 0;
+    }
+    else thread[i].time++;
+  }
+}
+
+void threadSetup(void (*callback)(), int threadInterval, ...) {
   va_list args;
   va_start(args, threadInterval);
 
-  while (va_arg(args, CallbackFunc))
-  {
-    va_arg(args, int);
-    numThread++;
-  }
-
-  // Dynamically allocate memory using malloc()
-  // threadTime     = (int *) malloc(numThread * sizeof(int));
-  // threadInterval = (int *) malloc(numThread * sizeof(int));
-  // threadFunc     = (CallbackFunc *) malloc(numThread * sizeof(CallbackFunc));
-
-  thread.time[0] = 0;
-  thread.func[0] = callback;
-  thread.interval[0] = threadInterval;
+  thread[0].time = 0;
+  thread[0].func = callback;
+  thread[0].interval = threadInterval;
 
   va_start(args, threadInterval);
-  for (int i = 1; i < numThread && i < TAMTHREAD; i++)
-  {
-    thread.time[i] = 0;
-    thread.func[i] = va_arg(args, CallbackFunc);
-    thread.interval[i] = va_arg(args, int);
+  for (int i = 1; i < NUMTHREADS; i++) {
+    thread[i].time = 0;
+    thread[i].func = va_arg(args, void (*)());
+    thread[i].interval = va_arg(args, int);
   }
   va_end(args);
-  cli();               // desliga as interrupções para não haver error durante a configuração
-  TCCR1A = 0b01000000; // modo comparação
-  TCCR1B = 0b00001011; // configuração do prescaler em 64 e OCR1A register is used to manipulate the counter resolution
-  TIMSK1 = 0b00000001; // habilita a interrupção por overflow do timer
-  OCR1A = 249;         // compare match register = [ 16,000,000Hz/ (prescaler * desired interrupt frequency) ] - 1
-  sei();               // habilita interrupções globais
-}
-
-ISR(TIMER1_OVF_vect)
-{
-  for (int i = 0; i < numThread && i < TAMTHREAD; i++)
-  {
-    if (thread.time[i] >= thread.interval[i])
-    {
-      thread.func[i]();
-      thread.time[i] = 0;
-    }
-    else
-      thread.time[i]++;
-  }
+  Timer1.initialize(1000); // Tempo em microsegundos da base das threads
+  Timer1.attachInterrupt(threads);  
 }
 
 
--- src/pisca_led_prof.ino
+++ src/pisca_led_prof.ino
@@ -1,57 +1,35 @@
 #include <Arduino.h>
+#define NUMTHREADS 2 //Deve-se sempre declarar o NUMTHREADS antes de "inindThread.h"
 #include "inindThread.h"
-#define ledPin 3 // the number of the LED pin
-#define analogPin A5
-#define LED_INTERVAL 500  // interval at which to blink (milliseconds)
-#define ANALOG_INTERVAL 1 // analog read interval (milliseconds)
 
-unsigned long previousMillisLED = 0;    // will store last time LED was updated
-unsigned long previousMillisAnalog = 0; // will store last time LED was updated
 unsigned long count = 0;
 
+#define LEDPIN 3 // the number of the LED pin
+#define LED_INTERVAL 500  // interval at which to blink (milliseconds)
 void ledFunc() // Faz a leitura do sinal Analogico
 {
-  digitalWrite(ledPin, !digitalRead(ledPin));
+  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
 }
 
+#define ANALOGPIN A5
+#define ANALOG_INTERVAL 1 // analog read interval (milliseconds)
 void analogReadFunc() // Faz a leitura do sinal Analogico
 {
   count += ANALOG_INTERVAL;
   Serial.print(">amp:");
   Serial.print(count);  
   Serial.print(":");  
-  Serial.print(analogRead(analogPin));
+  Serial.print(analogRead(ANALOGPIN));
   Serial.println("§Volts|g");
 }
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  pinMode(analogPin, INPUT);
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(ANALOGPIN, INPUT);
   Serial.begin(115200);
-  // threadSetup(ledFunc, LED_INTERVAL, analogReadFunc, ANALOG_INTERVAL, NULL);
+  threadSetup(ledFunc, LED_INTERVAL, analogReadFunc, ANALOG_INTERVAL);
 }
 
-void loop()
-{
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillisAnalog >= ANALOG_INTERVAL)
-  {
-    previousMillisAnalog = currentMillis;
-    analogReadFunc();
-  }
-  if (currentMillis - previousMillisLED >= LED_INTERVAL)
-  {
-    previousMillisLED = currentMillis;
-    ledFunc();
-  }
-}
+void loop() {}
 
-/* void loop()
-{
-    count++;
-    analogReadFunc();
-    ledFunc();
-    delay(LED_INTERVAL);
-    if(count % 100 == 0) delay(LED_INTERVAL);
-} */
