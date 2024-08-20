dec8bd18e21eca9eb7d8ee77991f23914f67c870
Rodrigo <rodrigotriboni@outlook.com>
2024-08-20 10:24:49 -0300

    refactor: uncomment libraries and changed test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,16 +1,20 @@
 #include <math.h>
 #include <Arduino.h>
 #include "Teleplot.h"
+#include "InIndKit.h"
 Teleplot teleplot("127.0.0.1");
 
 void setup() {
     Serial.begin(115200); 
     delay(500);
+    InIndKit.setup("Wokwi-GUEST", "", "inindkit10");
+
 
     
 }
 
 void loop() {
+    InIndKit.loop();
     // Send the sine and tangent values to Teleplot
     for (float i = 0; i < 1000; i += 0.1) {
         // Use instantiated object
@@ -20,3 +24,5 @@ void loop() {
         delay(10);  // delay in milliseconds
     }
 }
+
+
